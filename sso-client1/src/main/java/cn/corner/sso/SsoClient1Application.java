9c40a256e2e471dbe6de272fe26c6ba20501ec58
ConnorLH <1206350636@qq.com>
2018-10-08 10:50:21 +0800

    调整依赖支持SSO

--- sso-client1/src/main/java/cn/corner/sso/SsoClient1Application.java
+++ sso-client1/src/main/java/cn/corner/sso/SsoClient1Application.java
@@ -2,6 +2,7 @@ package cn.corner.sso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
